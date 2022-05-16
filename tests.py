2481605437275a30e7d2957851550a55632dc8ae
Aakashagr <aakashagr007@gmail.com>
2022-05-16 12:17:06 +0200

    Add files via upload

--- tests.py
+++ tests.py
@@ -12,6 +12,15 @@ import random
 # st.title('Displaying images')
 df = pd.read_csv('stimuli.csv')
 
+
+if 'data' not in st.session_state:
+    st.session_state['data'] = df.to_numpy()
+aud = st.empty()
+placeholder = st.empty()
+bid = np.zeros((9,1))
+
+
+
 # latest_iteration = st.empty()
 # bar = st.progress(0)
 
@@ -20,79 +29,64 @@ df = pd.read_csv('stimuli.csv')
 #   latest_iteration.text(f'Iteration {i+1}')
 #   bar.progress(i+10)
 
-if 'data' not in st.session_state:
-    st.session_state['data'] = df.to_numpy()
+
+# Display the button grid
+def btn_disp(stimorder,btn_dis = 0):
+    with placeholder.container():
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            for r in range(3):
+                bid[r+6] = st.button(stimorder[r], key = r, on_click = btn_state, args = (r,), disabled = btn_dis) 
+            
+        with col2:
+            for r in range(3):
+                bid[r+6] = st.button(stimorder[r+3], key = r+3,  on_click = btn_state, args = (r+3,), disabled = btn_dis) 
+                
+        with col3:
+            for r in range(3):
+                bid[r+6] = st.button(stimorder[r+6], key = r+6,  on_click = btn_state, args = (r+6,), disabled = btn_dis) 
+        
+
+def btn_state(r):
+    RT = time.time() - st.session_state['on_time'] 
+
+    if r == Tloc:
+        aud_file = "https://alphakhoj.s3.amazonaws.com/audio/a.mp3"
+
+    else:
+        aud_file = "https://alphakhoj.s3.amazonaws.com/audio/a.mp3"
+        st.session_state['data'] = np.r_[st.session_state['data'], [st.session_state['data'][0,:]]]
+
+    st.write(RT)
+    aud.markdown(f""" <audio autoplay>
+                <source src= {aud_file} type="audio/mpeg" />
+                Your browser does not support the audio element.
+                </audio>""", unsafe_allow_html=True)
+    
+    
+    st.session_state['data'] = np.delete(st.session_state['data'] , 0, 0)
+    np.random.shuffle(st.session_state['data'])   
+    btn_disp(stimorder,1)
+    time.sleep(1)
 
 
-if len(st.session_state['data']) > 0:    
+    
+if len(st.session_state['data']) > 0:   
+    
+    # Set trial conditions
     T = st.session_state['data'][0,0] # Target
     D = st.session_state['data'][0,1] # Distractor
-    Tloc = random.randint(0,8) # Target location
-    
+    Tloc = random.randint(0,8) # Target location  
     stimorder = np.tile(D,9)
     stimorder[Tloc] = T
 
-    def btn_state(r):
-        RT = time.time() - st.session_state['on_time'] 
-        st.write(st.session_state['data'])
-    
-        st.markdown("""
-                <audio autoplay>
-                <source src="a.mp3" type="audio/mpeg" />
-                Your browser does not support the audio element.
-                </audio>""", 
-                # <video controls width="250" autoplay="true" muted="true" loop="true">
-                # <source src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.webm" 
-                    # type="video/mp4" /> </video> """,
-                    unsafe_allow_html=True)
-
-
-        if r == Tloc:
-            st.title('Correct')
-        else:
-            st.title("wrong")
-            st.session_state['data'] = np.r_[st.session_state['data'], [st.session_state['data'][0,:]]]
-        
-        st.write(RT)
-        st.session_state['data'] = np.delete(st.session_state['data'] , 0, 0)
-        np.random.shuffle(st.session_state['data'])   
-        
-        
-
     st.session_state['on_time'] = time.time() # Start time of stimulus presentation
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        for r in range(3):
-            st.button(stimorder[r], key = r, on_click = btn_state, args = (r,)) 
-        
-    with col2:
-        for r in range(3):
-            st.button(stimorder[r+3], key = r+3,  on_click = btn_state, args = (r+3,))
-            
-    with col3:
-        for r in range(3):
-            st.button(stimorder[r+6], key = r+6,  on_click = btn_state, args = (r+6,))
+    btn_disp(stimorder)
 
 else:
-    st.write('Done')
-# st.write(st.session_state['bid'])
+    st.snow()
 
-# if sum(st.session_state['bid']):
-#     RT = time.time() - st.session_state['on_time']
-#     if np.where(st.session_state['bid'])[0] == 6:
-#         st.title('Correct')
-#     else:
-#         st.title("wrong")
 
-#     st.session_state['trial'] += 1
-#     st.write(RT)
-
-#     with col2:        
-#        nxt_btn = st.button('NEXT', on_click = store_data)
-#        st.session_state['btn-state'] = 0
-#        st.session_state['bid'] = np.zeros((9,1))
-
-    
 
 m = st.markdown("""
 <style>
